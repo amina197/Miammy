893287289ea87f287c7caf9dec5db4407fa5adc7
Amina <amina.thiam.pro@gmail.com>
2022-05-03 19:40:05 +0200

    Refactor Areas in its own file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,10 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import Areas from './Areas.jsx';
+
 
 export default function App(props) {
 
-  const [areas, setAreas] = useState([]);
 
-  // On app load get all areas
-  axios.get('/api/areas')
-    .then(({data}) => setAreas(data))
-    .catch(err => console.log('Error client retrieveing areas', err))
 
-  const allAreas = areas.map((area, i) => { return area.strArea !== 'Unknown' ? <li key={i}>{area.strArea}</li> : ''})
-
-  return (<ul>{allAreas}</ul>)
+  return (<Areas/>)
 }
---
+++ client/src/components/Areas.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+export default function Areas(props) {
+  const [areas, setAreas] = useState([]);
+
+  // On app load get all areas
+  axios.get('/api/areas')
+    .then(({data}) => setAreas(data))
+    .catch(err => console.log('Error client retrieveing areas', err))
+
+  const allAreas = areas.map((area, i) => { return area.strArea !== 'Unknown' ? <li key={i}>{area.strArea}</li> : ''})
+
+  return(<ul>{allAreas}</ul>)
+}
