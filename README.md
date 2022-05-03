b7aba7f4e24c0a3b0cfb56aa1d541a957fbfc15f
Amina <amina.thiam.pro@gmail.com>
2022-05-03 18:08:47 +0200

    Initialize react app and configure babel and webpack

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Nekhna
-Food recipe app to give inspiration
+Food recipe app to get inspiration
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: '/client/src/index.jsx',
+  output: {
+    path: path.join(__dirname, 'client', 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'client', 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  }
+}
