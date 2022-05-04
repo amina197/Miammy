6cd4a589d6f8fac6f6fa2b15859e10a3499b9eb0
Amina <amina.thiam.pro@gmail.com>
2022-05-04 03:03:42 +0200

    Fix request loop issue with useEffect

--- client/src/components/Areas.jsx
+++ client/src/components/Areas.jsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 export default function Areas({displayMeals}) {
   const [areas, setAreas] = useState([]);
 
-  // On app load get all areas
-  axios.get('/api/areas')
-    .then(({data}) => setAreas(data))
-    .catch(err => console.log('Error client retrieveing areas', err));
+  useEffect(() => {
+    // On app load get all areas
+    axios.get('/api/areas')
+      .then(({data}) => setAreas(data))
+      .catch(err => console.log('Error client retrieveing areas', err));
+  }, [])
 
   const clickedArea = (e) => {
     axios.get(`api/meals?area=${e.target.innerText}`)
--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 export default function Categories({displayMeals}) {
   const [categories, setCategories] = useState([]);
 
-  // On app load get all areas
-  axios.get('/api/categories')
-    .then(({data}) => setCategories(data))
-    .catch(err => console.log('Error client retrieveing categories', err));
+  useEffect(() => {
+    // On app load get all categories
+    axios.get('/api/categories')
+      .then(({data}) => setCategories(data))
+      .catch(err => console.log('Error client retrieveing categories', err));
+  }, [])
 
   const clickedCategory = (e) => {
     axios.get(`api/meals?category=${e.target.innerText}`)
@@ -15,7 +17,7 @@ export default function Categories({displayMeals}) {
       .catch(err => console.log('Error client retrieveing meals by categories', err));
   };
 
-  const allCategories = categories.map((category, i) => { return category.strCategory !== 'Unknown' ? <li onClick={clickedArea} key={i}>{category.strCategory}</li> : ''})
+  const allCategories = categories.map((category, i) => { return category.strCategory !== 'Unknown' ? <li onClick={clickedCategory} key={i}>{category.strCategory}</li> : ''})
 
   return(<ul>{allCategories}</ul>)
 }
