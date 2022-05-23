864ab4b5d8bfc02fe252da81c886f39bc5533b39
Amina <amina.thiam.pro@gmail.com>
2022-05-23 16:43:22 +0200

    Refactor Landing in its own file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
-import { useNavigate } from 'react-router-dom';
-import { isLoggedIn, showSignIn } from '../atoms';
 import GlobalStyle from '../styled/globalStyles';
-import Header from './Header';
+import Landing from './Landing';
 import SignIn from './SignIn';
-import { SLandingWrapper, SLandingSlider } from '../styled/S-Landing';
 
 export default function App() {
-  const [isLogged, setIsLogged] = useRecoilState(isLoggedIn);
-  const [showModal, setShowModal] = useRecoilState(showSignIn);
-  const navigate = useNavigate();
-
-  const handleDiscoverClick = () => {
-    if (isLogged) {
-      navigate('/meals');
-    } else {
-      setShowModal(true);
-    }
-  };
-
   return (
     <>
       <GlobalStyle />
-      <SLandingWrapper>
-        <Header />
-        <SLandingSlider>
-          <h1>Miammy</h1>
-          <h2>Get recipes ideas from around the world</h2>
-          <button type="button" onClick={handleDiscoverClick}>Discover</button>
-        </SLandingSlider>
-      </SLandingWrapper>
+      <Landing />
       <SignIn />
     </>
   );
